a43af10e87e0538e23d0181d48f4861ae2c7fa41
Dev.Hwang <zxc1063@naver.com>
2022-07-31 04:35:57 +0900

    UPROPERTY

--- Source/UnrealStudy/MyActor.h
+++ Source/UnrealStudy/MyActor.h
@@ -23,4 +23,11 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* Mesh;
+
+	UPROPERTY(EditAnywhere, Category="BattleStat")	
+	int32 Hp;
+	UPROPERTY(EditAnywhere, Category="BattleStat")
+	int32 Mp;
 };
